c4f17638204ea4bda671c408fe6ebe1d997c7f97
Denzel <denzelsamala@Denzel.local>
2021-11-24 09:56:47 -0800

    aligned elements to be redesigned according to wireframe

--- api-test/js/script.js
+++ api-test/js/script.js
@@ -3,18 +3,12 @@ async function getWeather(e) {
 
 	const userInput = $('input[type="text"]').val();
 	const apiKey = "9668a116beecace6125596e0cd70a7da";
-	// const url = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=9668a116beecace6125596e0cd70a7da';
-		const url = 'http://api.openweathermap.org/data/2.5/weather?q='+ userInput + '&units=imperial' + '&APPID='+ apiKey;
+	const url = 'http://api.openweathermap.org/data/2.5/weather?q='+ userInput + '&units=imperial' + '&APPID='+ apiKey;
 
 	const response = await fetch(url);
 	const data = await response.json();
 	
-	
-	// const image = $('img').appendTo('#image_container');
-	// image.attr('src', image_url)
-	// const $img = $('<img>').attr('src', image_url);
-	// $('#image_container').append($img);
-	$('#city').html('City ' + data.name);
+	$('#city').html('City: ' + data.name);
 	$('#description').html('Weather: ' + data.weather[0].description);
 	$('#tempmin').html('Lowest: ' + data.main.temp_min + '°');
 	$('#tempmax').html('Highest: ' + data.main.temp_max + '°');
