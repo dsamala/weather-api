67fd96b89813a55d55c22fe8030d3bbc567059b2
Denzel <denzelsamala@Denzel.local>
2021-11-24 15:01:46 -0800

    added specific detail and random detail options

--- api-test/js/script.js
+++ api-test/js/script.js
@@ -7,7 +7,7 @@ async function getWeather(e) {
 
 	const response = await fetch(url);
 	const data = await response.json();
-	
+		
 	$('#city').html('City: ' + data.name);
 	$('#description').html('Weather: ' + data.weather[0].description);
 	$('#tempmin').html('Lowest: ' + data.main.temp_min + '°');
@@ -16,3 +16,58 @@ async function getWeather(e) {
 
 $("form").on("submit", getWeather);
 
+
+
+
+async function getCityWeather(e) {
+	e.preventDefault();
+
+	const userInput = $('button').val();
+	const apiKey = "9668a116beecace6125596e0cd70a7da";
+	const url = 'http://api.openweathermap.org/data/2.5/weather?q='+ userInput + '&units=imperial' + '&APPID='+ apiKey;
+
+	const response = await fetch(url);
+	const data = await response.json();
+		
+	$('#citydescription').html('Weather: ' + data.weather[0].description);
+	$('#citytempmin').html('Lowest: ' + data.main.temp_min + '°');
+	$('#citytempmax').html('Highest: ' + data.main.temp_max + '°');
+};
+
+$("#sj").on("click", getCityWeather);
+$("#sf").on("click", getCityWeather);
+$("#oak").on("click", getCityWeather);
+
+
+
+
+
+
+
+async function getRandom(e) {
+	e.preventDefault();
+
+	
+	const userInput = $('input[type="text"]').val();
+	const apiKey = "9668a116beecace6125596e0cd70a7da";
+	const url = 'http://api.openweathermap.org/data/2.5/weather?q='+ userInput + '&units=imperial' + '&APPID='+ apiKey;
+
+	
+	const response = await fetch(url);
+	const data = await response.json();
+
+	
+	const objectProps = ['City Latitude: ' + data.coord.lat, 'City Longitude: ' + data.coord.lon, 'City ID: ' + data.id, 'City Wind Speed: ' + data.wind.speed];
+	let randomProp = '';
+
+	function getRandomProp () {
+		let randomIndex = Math.floor(Math.random() * objectProps.length);
+		randomProp = objectProps[randomIndex];
+	};
+
+	getRandomProp();
+	$('.rinfo').html('Random Info: ' + randomProp);
+
+};
+
+$(".rbutton").on('click', getRandom);
